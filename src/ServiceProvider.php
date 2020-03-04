0deac4873b97f68f88aaabd123a55a00e9d2f5d2
VAGH <yu@vagh.cn>
2020-03-04 23:59:59 +0800

    support laravel 7.x

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -8,10 +8,26 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
     public function boot()
     {
-        $this->app['swift.transport']->extend('ali_mail', function () {
+    	$manager_name = $this->getTransportManager();
+
+        $this->app[$manager_name]->extend('ali_mail', function ($app) {
             $config = $this->app['config']->get('services.ali_mail', []);
 
             return new MailSender($config);
         });
+    }
+
+    /**
+     * Resolve the mail manager.
+     *
+     * @return \Illuminate\Mail\TransportManager|\Illuminate\Mail\MailManager
+     */
+    public function getTransportManager()
+    {
+        if ($this->app->has('mail.manager')) {
+            return 'mail.manager';
+        }
+
+        return 'swift.transport';
     }
 }
